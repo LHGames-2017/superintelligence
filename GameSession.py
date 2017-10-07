95ddf4f4b7fb163915860484a0e147b7474e9fa7
geekymoose <constantin.masson@umontreal.ca>
2017-10-07 15:33:50 -0400

    Add GameSession

---
+++ GameSession.py
@@ -0,0 +1,5 @@
+
+class GameSession:
+    playerSession = None
+    def __init__(self):
+        self.frameCounter = 0
--- ai.py
+++ ai.py
@@ -6,8 +6,11 @@ import numpy
 
 from gameHelper import *
 from PlayerSession import *
+from GameSession import *
 
 app = Flask(__name__)
+gameSession = GameSession()
+gameSession.playerSession = PlayerSession(None)
 
 def create_action(action_type, target):
     actionContent = ActionContent(action_type, target.__dict__)
@@ -71,13 +74,13 @@ def bot():
                     p["MaxHealth"],
                     Point(x,y),
                     Point(house["X"], house["Y"]),
+                    p["Score"],
                     p["CarriedResources"],
                     p["CarryingCapacity"])
 
     # Map
     serialized_map = map_json["CustomSerializedMap"]
     deserialized_map = deserialize_map(serialized_map)
-
 
     # Print
     print_game(deserialized_map, player)
@@ -94,21 +97,23 @@ def bot():
 
             otherPlayers.append({player_name: player_info })
 
-    pos = PathFinder(deserialized_map).getPath(player.Position, player.Position + Point(3,3))[0]
-    print "From {}".format(player.Position)
-    print "To {}".format(pos)
 
     # return decision
-    carryHome(player)
-    scanNeighbourhood(deserialized_map)
+    target = scanNeighbourhood(deserialized_map, player)
+    pos = PathFinder(deserialized_map).getPath(player.Position, target)[0]
+
+    gameSession.frameCounter += 1
+    print(gameSession.frameCounter)
     return create_move_action(Point(0,1))
 
-def scanNeighbourhood(deserialized_map):
-    for i in range(len(deserialized_map)):
-        for j in range(len(deserialized_map[0])):
-            if deserialized_map[i][j].Content == TileContent.Resource:
-                return create_move_action(Point(i, j))
-    # TODO move if there is no resource on the deserialized map
+def scanNeighbourhood(deserialized_map, player):
+    for y in range(len(deserialized_map)):
+        for x in range(len(deserialized_map[0])):
+            if deserialized_map[y][x].Content == 4:
+                tile = deserialized_map[y][x]
+                return Point(tile.X, tile.Y)
+                #return player.Position + Point(0,1)
+    return Point(25, 25)
 
 def carryHome(player):
 
@@ -124,5 +129,4 @@ def reponse():
     return bot()
 
 if __name__ == "__main__":
-    playerSession = PlayerSession(None)
     app.run(host="0.0.0.0", port=8080)
